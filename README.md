a25e5ec0fa40192086cb8ee30443143d8d82edda
Schm1tz1 <ro-man@web.de>
2019-11-30 20:09:06 +0100

    Fixed pinout and added simple send/receive example for TTGO ESP32 SX1276 v1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # TTGO-LoRa-ESP32-SX1276-LoRa
-Code example for TTGO ESP32 SX1276 LoRa 868 / 915MHz Bluetooth WIFI Lora
+Code example for TTGO ESP32 SX1276 LoRa 868 / 915MHz Bluetooth WIFI Lora using the LoRa library by sandeepmistry.
+
+Pins are configured for the v1 board but can be adapted for newer versions. 
+More information can be found here: https://github.com/manuelbl/ttn-esp32/wiki/Boards-and-Pins#ttgo-lora32-868mhz-v1-sx1276-with-and-without-display
--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,70 @@
 //
 
 #include <Arduino.h>
+#include "PinsTTGO.h"
+#include "LoRa.h"
+
+#define LORA_BAND    868E6  //you can set band here directly,e.g. 868E6,915E6,433E6
+
+int counter = 0;
 
 void setup() {
+    Serial.begin(9600);
+    while (!Serial) {};
 
+    Serial.println("LoRa Dump Registers");
+
+    // override the default CS, reset, and IRQ pins (optional)
+    LoRa.setPins(TTGO_PIN_NSS, TTGO_PIN_RST, TTGO_PIN_DIO0); // set CS, reset, IRQ pin
+
+    if (!LoRa.begin(LORA_BAND)) {         // initialize ratio at 915 MHz
+        Serial.println("LoRa init failed. Check your connections.");
+        while (true);                   // if failed, do nothing
+    }
+
+    LoRa.dumpRegisters(Serial);
+}
+
+void receivePacket() {
+    // try to parse packet
+    int packetSize = LoRa.parsePacket();
+    if (packetSize) {
+        // received a packet
+        Serial.print("Received packet '");
+
+        // read packet
+        while (LoRa.available()) {
+            Serial.print((char) LoRa.read());
+        }
+
+        // print RSSI of packet
+        Serial.print("' with RSSI ");
+        Serial.println(LoRa.packetRssi());
+        Serial.print("' and SNR ");
+        Serial.println(LoRa.packetSnr(), 3);
+    }
+}
+
+void sendPacket(int waitInMillis) {
+
+    while (LoRa.beginPacket() == 0) {
+        Serial.print("waiting for radio ... ");
+        delay(waitInMillis);
+    }
+
+    Serial.print("Sending packet non-blocking: ");
+    Serial.println(counter);
+
+    // send packet
+    LoRa.beginPacket();
+    LoRa.print("Hello LoRa #");
+    LoRa.print(counter);
+    LoRa.endPacket(true);
+
+    counter++;
 }
 
 void loop() {
-
+    //sendPacket(100);
+    receivePacket();
 }
